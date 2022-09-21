aa5cefea27184cdb41344374dda54ed62fb15998
huangfei <1280707080@qq.com>
2022-09-21 22:52:09 +0800

    update application

--- application.go
+++ application.go
@@ -1,6 +1,15 @@
 package tuago
 
+import "github.com/gin-gonic/gin"
+
 type AppEnvPropertyHook func(dest map[string]string) error
 
 type Application struct {
+	*gin.Engine
+	envProperties       map[string]string    // 环境变量
+	appEnvPropertyHooks []AppEnvPropertyHook // 环境变量钩子
+
+	tableChain []TableInf
+	cleanChain []CleanAble
+	initChain  []InitAble
 }
